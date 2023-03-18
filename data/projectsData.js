6ff04a1b5b5b734c389ba65d18d2b48f56be669e
Juan Gonzalez <lujujose@gmail.com>
2023-03-17 23:24:09 -0700

    initial commit

---
+++ data/projectsData.js
@@ -0,0 +1,9 @@
+const projectsData = [
+  {
+    title: 'This Website!',
+    description: `A website where resources regarding Sex Ed and Reporoductive Health meet.`,
+    imgSrc: '/static/images/logo.png',
+    href: 'https://c5seahr.vercel.app',
+  },
+]
+export default projectsData
